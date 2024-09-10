b5ac31754f8d10950ad15819006bcbb9899f4e47
Anthony Payton <paytonanthony2020@gmail.com>
2024-09-10 13:16:54 -0600

    Building Card class and access with RSpec

---
+++ card/card.rb
@@ -0,0 +1,21 @@
+
+class Card
+  
+  attr_accessor :suit, :rank, :show #allows access, first 3 pass 
+  SUITS = %w[Hearts Spades Clubs Diamonds]
+  RANKS = %w[Ace 2 3 4 5 6 7 8 9 10 Jack Queen King]
+
+  def initialize suit, rank
+    @show = true
+    
+    if SUITS.include?(suit) && RANKS.include?(rank)
+      @suit = suit
+      @rank = rank
+    else
+      @suit = 'unknown'
+      @rank = 'unkown'
+    end
+
+  end
+
+end
